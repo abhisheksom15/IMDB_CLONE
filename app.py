18c8c9322e10573727878307cadb2a83386bae9f
abhishek <abhishek.som.cs.2015@miet.ac.in>
2021-02-08 15:52:51 +0530

    DB Changed to MySQL

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.debug = False
 app.config['SECRET_KEY'] = '8b1e36fe-5751-4d53-b4bd-e3e008517c50'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///imdb.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://b407c49e05ce2f:253ea132@us-cdbr-east-03.cleardb.com/heroku_e5aef511337d168"
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path=os.path.join(os.path.abspath(os.path.dirname(__file__)),"search.db")
 #app.config['WHOOSH_BASE']=path
 app.config['ADMIN_KEY']="cb993ada-d3ae-43ee-aad0-3a2ec50a70b0"
 
